5a0b20fe271b412a453e2f86ba10101e0706947d
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2021-05-18 17:08:50 +0100

    Simplify HasRole

--- app/Http/Middleware/HasRole.php
+++ app/Http/Middleware/HasRole.php
@@ -17,9 +17,7 @@ class HasRole
     public function handle($request, Closure $next, string $role)
     {
         $user = $request->user();
-        //Because this should have been checked by an earlier middleware
-        if (!$user) abort(500, 'Server error - HasRole checked with no user set.');
-        if (!$user->hasRole($role)) {
+        if (!$user || !$user->hasRole($role)) {
             abort(403);
         }
         return $next($request);
