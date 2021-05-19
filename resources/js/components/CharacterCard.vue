c50ad7e6ff21101a375296e2b60acecd5fb1ae78
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2021-05-19 18:49:40 +0100

    Flesh out character select

--- resources/js/components/CharacterCard.vue
+++ resources/js/components/CharacterCard.vue
@@ -1,97 +1,105 @@
 <template>
-    <div class="card character-card" :class="'mode-' + mode">
-        <a :href="'/p/' + character.name">
-            <div class="card-body">
-                <div class="avatar"></div>
-                <div class="name">{{ character.name }}</div>
-                <div class="level">{{ character.level }}</div>
-                <div v-if="character.wizard" class="staff">Staff</div>
-            </div>
-        </a>
+    <div class="card character-card" :class="'mode-' + mode" @click="clicked">
+        <div class="card-body">
+            <div class="avatar"></div>
+            <div class="name">{{ character.name }}</div>
+            <div class="level">{{ character.level }}</div>
+            <div v-if="character.wizard" class="staff">Staff</div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: "character-card",
-        props: {
-            character: {type: Object, required: true},
-            mode: {type: String, default: 'tag'}
-        },
-        data: function () {
-            return {}
-        },
-        methods: {}
+export default {
+    name: "character-card",
+    props: {
+        character: {type: Object, required: true},
+        mode: {type: String, default: 'tag'}
+    },
+    data: function () {
+        return {}
+    },
+    methods: {
+        clicked: function() {
+            if (this.$listeners.click)
+                this.$emit('click');
+            else
+                window.location = '/c/' + this.character.name;
+        }
     }
+}
 </script>
 
 <style scoped lang="scss">
-    // Shared in all modes
-    .card-body {
-        position:absolute;
-        left:1px;
-        right:1px;
-        top:1px;
-        bottom:1px;
-        background:red;
+// Shared in all modes
+.card-body {
+    position: absolute;
+    left: 1px;
+    right: 1px;
+    top: 1px;
+    bottom: 1px;
+    background: red;
+}
+
+.character-card {
+    cursor: pointer;
+}
+// Tag Mode
+.character-card.mode-tag {
+    width: 240px;
+    height: 100px;
+    box-shadow: 2px 2px 2px #888888;
+
+    .avatar {
+        position: absolute;
+        z-index: 1;
+        left: 0;
+        top: 0;
+        bottom: 0;
+        width: 192px;
+        background: #3490dc;
     }
 
-    // Tag Mode
-    .character-card.mode-tag {
-        width: 240px;
-        height: 100px;
-        box-shadow: 2px 2px 2px #888888;
-
-        .avatar {
-            position: absolute;
-            z-index: 1;
-            left: 0;
-            top: 0;
-            bottom: 0;
-            width: 192px;
-            background: #3490dc;
-        }
-
-        .name {
-            position: absolute;
-            z-index: 2;
-            right: 3px;
-            top: 3px;
-            width: 124px;
-            height: 40px;
-            background: navy;
-            color: white;
-            text-align: right;
-            font-weight: bold;
-            font-size: large;
-            font-family: Impact, Haettenschweiler, "Franklin Gothic Bold", Charcoal, "Helvetica Inserat", "Bitstream Vera Sans Bold", "Arial Black", sans serif;
-        }
-
-        .level {
-            position: absolute;
-            z-index: 2;
-            left: 198px;
-            right: 3px;
-            top: 48px;
-            bottom: 3px;
-            background: darkcyan;
-            color: white;
-            text-align: center;
-        }
-
-        .staff {
-            position: absolute;
-            z-index: 2;
-            width: 98px;
-            height: 24px;
-            color: black;
-            text-align: center;
-            top: 35px;
-            left: 12px;
-            background: cornsilk;
-            transform: translateX(-50%) rotate(-90deg);
-        }
-
+    .name {
+        position: absolute;
+        z-index: 2;
+        right: 3px;
+        top: 3px;
+        width: 124px;
+        height: 40px;
+        background: navy;
+        color: white;
+        text-align: right;
+        font-weight: bold;
+        font-size: large;
+        font-family: Impact, Haettenschweiler, "Franklin Gothic Bold", Charcoal, "Helvetica Inserat", "Bitstream Vera Sans Bold", "Arial Black", sans serif;
     }
+
+    .level {
+        position: absolute;
+        z-index: 2;
+        left: 198px;
+        right: 3px;
+        top: 48px;
+        bottom: 3px;
+        background: darkcyan;
+        color: white;
+        text-align: center;
+    }
+
+    .staff {
+        position: absolute;
+        z-index: 2;
+        width: 98px;
+        height: 24px;
+        color: black;
+        text-align: center;
+        top: 35px;
+        left: 12px;
+        background: cornsilk;
+        transform: translateX(-50%) rotate(-90deg);
+    }
+
+}
 
 </style>
--- resources/js/components/CharacterSelect.vue
+++ resources/js/components/CharacterSelect.vue
@@ -1,5 +1,23 @@
 <template>
-
+    <div class="card">
+        <h4 class="card-header">Select Character</h4>
+        <div class="card-body">
+            <p class="text-center">You need to select an active character before continuing.</p>
+            <div class="mb-2">
+                <div v-for="character in characters" class="row">
+                    <div class="col-lg-auto">
+                        <character-card :character="character" mode="tag"
+                                        @click="setActiveCharacter(character.dbref)"></character-card>
+                    </div>
+                </div>
+            </div>
+            <div class="text-center">
+                <p>Alternatively you can manage your characters, including creating a new one, from the character
+                    dashboard.</p>
+                <a class="btn btn-primary" href="/multiplayer/home">Character Dashboard</a>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -11,7 +29,17 @@ export default {
     data: function () {
         return {}
     },
-    methods: {}
+    methods: {
+        setActiveCharacter: (dbref) => {
+            let promise = axios.post('/account/setactivecharacter', {dbref: dbref});
+            return promise
+                .then(response => {
+                    if (response.data.redirectUrl) location.replace(response.data.redirectUrl);
+                    else location.reload();
+                });
+
+        }
+    }
 }
 </script>
 
