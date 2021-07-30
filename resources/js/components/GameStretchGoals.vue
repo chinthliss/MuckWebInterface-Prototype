d0a9f1d5880b606949b83892c62c0efff319b7e1
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2021-07-30 14:48:42 +0100

    Replaced text-secondary with text-muted since it's too dark.

--- resources/js/components/GameStretchGoals.vue
+++ resources/js/components/GameStretchGoals.vue
@@ -4,7 +4,7 @@
         As the monthly contributions hit certain values, new things will be added to the game for all to enjoy.
         <div class="goal-next" v-if="progressToNext">{{ progressToNext }} dollars to the next goal!</div>
         <div class="goal-container">
-            <div class="goal" v-bind:class="[ amount <= progress ? 'text-primary' : 'text-secondary' ]"
+            <div class="goal" v-bind:class="[ amount <= progress ? 'text-primary' : 'text-muted' ]"
                  v-for="(goal,amount) in goals">
                 <i class="goal-lock fas" v-bind:class="[ amount < progress ? 'fa-lock-open' : 'fa-lock' ]"></i>
                 <span class="goal-amount">${{ amount }}</span>
