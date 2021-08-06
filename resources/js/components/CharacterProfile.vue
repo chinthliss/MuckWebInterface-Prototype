0af0a782ec5e6440b880b405ebbc64864e9b8a71
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2021-08-06 16:44:55 +0100

    Fix missing 'To be Completed'

--- resources/js/components/CharacterProfile.vue
+++ resources/js/components/CharacterProfile.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>Pending</div>
+    <div class="container">
+        <div class="row">
+            <div class="col text-center">
+                <h1><i class="fas fa-hammer"></i> To Be Completed <i class="fas fa-hammer fa-flip-horizontal"></i></h1>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
