8462c6001baaac07289b359bb4c443799c1b060b
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2021-09-02 16:26:20 +0100

    Bump up Patreon's full_name width

--- database/migrations/2020_12_23_171049_create_patreon_users_table.php
+++ database/migrations/2020_12_23_171049_create_patreon_users_table.php
@@ -16,7 +16,7 @@ class CreatePatreonUsersTable extends Migration
         Schema::create('patreon_users', function (Blueprint $table) {
             $table->integer('patron_id')->unsigned()->primary();
             $table->string('email')->index()->nullable();
-            $table->string('full_name', 100)->nullable();
+            $table->string('full_name', 120)->nullable();
             $table->string('vanity', 100)->nullable();
             $table->boolean('hide_pledges')->nullable();
             $table->string('url')->nullable();
