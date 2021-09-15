a1a9082064758accf750eaf965fc69117fae844b
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2021-09-15 16:27:05 +0100

    Create Muck Objects cache table

---
+++ database/migrations/2021_09_01_154811_create_muck_objects_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMuckObjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('muck_objects', function (Blueprint $table) {
+            $table->id('id');
+            $table->tinyInteger('game_code');
+            $table->integer('dbref');
+            $table->timestamp('created_at');
+            $table->enum('type', ['player', 'room', 'thing']);
+
+            $table->bigInteger('aid')->nullable();
+            $table->string('name', 255)->nullable();
+            $table->timestamp('deleted_at')->nullable()
+                ->comment('Only tracked for player objects, everything else is just deleted from this table.');
+
+            $table->index(['game_code', 'dbref', 'created_at']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('muck_objects');
+    }
+}
