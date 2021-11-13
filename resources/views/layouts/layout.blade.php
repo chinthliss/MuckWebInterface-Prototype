557463d782b399fc1d647076fd7b433741ef210c
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2021-11-13 04:02:44 +0000

    Add OpenGraph meta tags

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -3,25 +3,23 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:site_name" content="{{ config('app.name', 'MuckWebInterface') }}">
+    <meta property="og:image" content="{{ url('/favicon32.png') }}">
+    @hasSection('title')<meta property="og:title" content="@yield('title')">@endif
+    <meta property="og:description" content="Under development beta site.">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <!-- Account if logged in -->
-    @auth
-    <meta name="account-id" content="{{ Auth::user()->getAid() }}">
-    @endauth
+    @auth<meta name="account-id" content="{{ Auth::user()->getAid() }}">@endauth
 
     <!-- Character if set -->
-    @Character
-    <meta name="character-dbref" content="{{ Auth::user()->getCharacterDbref() }}">
-    @endCharacter
+    @Character<meta name="character-dbref" content="{{ Auth::user()->getCharacterDbref() }}">@endCharacter
 
     <!-- Title -->
-    @hasSection('title')
-        <title>@yield('title') ({{ config('app.name', 'MuckWebInterface') }})</title>
-    @else
-        <title>{{ config('app.name', 'MuckWebInterface') }}</title>
+    @hasSection('title')<title>@yield('title') ({{ config('app.name', 'MuckWebInterface') }})</title>
+    @else<title>{{ config('app.name', 'MuckWebInterface') }}</title>
     @endif
 
     <!-- Scripts -->
