5b6d88e6bedfcd037da7e53ea57bb417ec04c670
chinthliss <55845146+chinthliss@users.noreply.github.com>
2022-06-09 00:48:44 +0100

    Update roadmap

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,57 +22,52 @@ class HomeController extends Controller
             [
                 'title' => 'Styling/Theming',
                 'description' => 'Build up a common set of styles to use throughout the site. This includes looking at the page layout and how it breaks down on mobile/tablets.',
-                'progress' => 'Ongoing'
+                'progress' => 'Prototype done. Need to revisit in final version.'
             ],
             [
                 'title' => 'Browser Notifications',
                 'description' => 'Allow game notifications to become browser notifications. This would be opt-in from the notifications screen rather than popping up on visiting the site. ',
-                'progress' => 'Evaluating desirability, may delay till later'
+                'progress' => 'Delayed until later.'
             ],
             [
                 'title' => 'Socialite for third party logins',
                 'description' => 'Allow logins from things like Facebook or Twitter.',
-                'progress' => 'Evaluating desirability, may delay till later'
+                'progress' => 'Delayed until later.'
             ],
             [
                 'title' => 'Payment integrations',
                 'description' => 'Overhaul of the payment system to unify it and to bring it up to date with latest vendor requirements.',
-                'progress' => 'Done but pending being able to test extensively'
+                'progress' => 'Done but pending being able to test extensively.'
             ],
             [
                 'title' => 'Patreon Integration',
                 'description' => 'Allow patreon support to give a bonus in-game. Re-write of the existing system (which is updated by manual tasks and requires manual claiming) to automate updates AND claiming.',
-                'progress' => 'All set up. Needs automation'
+                'progress' => 'All set up. Needs automation.'
             ],
             [
                 'title' => 'Connect',
                 'description' => 'Allow a webpage to control the muck via websockets. Re-write of the existing system which uses Flash. Also want to tie into this framework so it logs in as the active character.',
-                'progress' => 'Pending writing a client for though a simple version is now in use on the existing website.'
+                'progress' => 'Pending writing a client, though a simple version is now in use on the existing website.'
             ],
             [
                 'title' => 'Websocket',
                 'description' => 'Rewrite of the present websocket connection to the muck to work within this framework. Also stripping off backup websocket functionality since websockets are now prevalent. ',
-                'progress' => 'Pending'
+                'progress' => 'Pending.'
             ],
             [
                 'title' => 'Ticket System',
                 'description' => 'Rewrite of the ticket system to allow it to work separate to the muck and more cohesively from the web side.',
-                'progress' => 'Done bar muck side functionality and rewrite of how voting works so it isn\'t immutable.'
-            ],
-            [
-                'title' => 'Character Ordering',
-                'description' => 'Allow the order of characters on character select to be modified - this was requested several times in the past.',
-                'progress' => 'Pending'
+                'progress' => 'Done but has some outstanding tasks/changes: Muck side functionality; rewrite of how voting works so it isn\'t immutable; make suggestions their own top level category'
             ],
             [
                 'title' => 'Character Dashboard',
-                'description' => "Screen to show all of a player's characters along with live tracking of stats.",
+                'description' => "Screen to show all of a player's characters along with live tracking of stats. Allow re-ordering of characters",
                 'progress' => 'Started, presently just a list of characters'
             ],
             [
                 'title' => 'Character Profile',
                 'description' => "View somebody else's character. Note to self - look at including pinfo and view.",
-                'progress' => 'Created but barebone'
+                'progress' => 'Done bar further improvements / additions'
             ],
             [
                 'title' => 'Avatar editing',
