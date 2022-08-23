10892e933f20aefe71378e9d72a25f8a069eb1af
Chinthliss <55845146+chinthliss@users.noreply.github.com>
2022-08-23 12:43:58 +0100

    Partial implementation of testWebsocket

--- resources/js/components/TestWebsocket.vue
+++ resources/js/components/TestWebsocket.vue
@@ -3,10 +3,15 @@
         <h2>Test</h2>
         <div class="row">
             <div id="ChatHistory" class="col-10 border rounded">
-                <div v-for="message in history" class="message"><span class="user">{{ message.name }}</span>{{ message.message}}</div>
+                <div v-for="message in history" class="message" v-bind:class="[
+                        message.sameAsLast ? 'message-same-user' : '',
+                        message.name === this.userName ? 'message-self' : ''
+                    ]">
+                    <span class="user">{{ message.name }}</span>{{ message.message }}
+                </div>
             </div>
             <div id="ChatUserList" class="col-2 border rounded">
-                <div v-for="user in users">{{user}}</div>
+                <div v-for="user in users" class="user">{{ user }}</div>
             </div>
         </div>
     </div>
@@ -14,13 +19,16 @@
 
 <script>
 export default {
-    data: function() {
+    data: function () {
         return {
             history: [ // In the form {name, message, sameAsLast}
-                {name:'system', message:'Waiting on connection..', sameAsLast: false}
+                {name: 'system', message: 'Waiting on connection..', sameAsLast: false}
             ],
-            users:[],
-            channel: null
+            users: [],
+            userName: null,
+            channel: null,
+            lastUser: 0,
+            connected: false
         }
     },
     name: "test-websocket",
@@ -28,34 +36,77 @@ export default {
         MwiWebsocket.init();
         this.channel = MwiWebsocket.channel('test-websocket');
 
-        this.channel.on('player-joined', (data) => {
-            this.users.push(data);
+        this.channel.on('connected', () => {
+            this.connected = true;
+            this.userName = MwiWebsocket.getPlayerName();
+            this.history.push({name: 'system', message: 'Connected', sameAsLast: false});
         });
 
-        this.channel.on('player-left', (data) => {
-            const index = this.users.indexOf(data);
-            if (index !== -1) this.users.splice(index, 1);
+        this.channel.on('disconnected', () => {
+            this.connected = false;
+            this.userName = null;
+            this.history.push({name: 'system', message: 'Disconnected', sameAsLast: false});
+        });
+
+        this.channel.on('player-list', (data) => {
+            this.users = data;
+        });
+
+        // Expecting [player, playerName, message]
+        this.channel.on('message', (data) => {
+            if (typeof data !== 'object') throw "Unexpected data in chat message";
+            let [player, playerName, message] = data;
+            const chatOutput = $('#ChatHistory');
+            let safeParse = $('<div></div>');
+            safeParse.text(message);
+            this.history.push({name: playerName, message: message, sameAsLast: player === this.lastUser});
+            chatOutput.parent().scrollTop(chatOutput.parent()[0].scrollHeight);
+            this.lastUser = player;
         });
     }
 }
 </script>
 
 <style scoped>
-    #ChatHistory {
-        min-height:500px;
-        background: mintcream;
-    }
+#ChatHistory {
+    min-height: 500px;
+    background: mintcream;
+}
 
-    #ChatUserList {
-        min-height:500px;
-        background: mintcream;
-    }
+#ChatUserList {
+    min-height: 500px;
+    background: mintcream;
+}
 
-    .message:first-child { border-top:none; }
-    .message { border-top:1px solid gray; color:black; }
-    .message-same-user { border-top:1px dashed gray; }
-    .message-self { background-color: cornsilk; }
-    .message span { font-weight:bold; margin-right:8px; color:navy; }
-    .message-self span { color:darkgreen; }
+.message:first-child {
+    border-top: none;
+}
+
+.message {
+    border-top: 1px solid gray;
+    color: black;
+}
+
+.message-same-user {
+    border-top: 1px dashed gray;
+}
+
+.message-self {
+    background-color: cornsilk;
+}
+
+.message span {
+    font-weight: bold;
+    margin-right: 8px;
+    color: navy;
+}
+
+.message-self span {
+    color: darkgreen;
+}
+
+.user {
+    color: black;
+}
 
 </style>
